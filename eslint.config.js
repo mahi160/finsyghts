6950229cf08b7d8ab15cec411dc3b4ae78f870db
mahi160 <omarsifat288@gmail.com>
2025-10-08 21:15:24 +0600

    chore: project base setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,9 @@
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import { defineConfig } from 'eslint/config'
 
-export default [...tanstackConfig]
+export default defineConfig([
+  ...tanstackConfig,
+  { ignores: ['commitlint.config.js'] },
+])
